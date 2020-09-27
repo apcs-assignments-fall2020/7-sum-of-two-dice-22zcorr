948558b4b7923d67047fc59963f42437fc217550
22zcorr <22zcorr@athenian.org>
2020-09-27 17:26:10 +0000

    I wrote the HW code

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -5,20 +5,30 @@ public class MyMain {
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
-        // REPLACE YOUR CODE HERE
-        return -1;
+       return ((int) ((Math.random()*6) +1)) ;
     }
 
     // Rolls two dice n times
     // This method returns an int array of size 11, where each element is
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
-        // REPLACE YOUR CODE HERE
-        return null;
+        int[] rolls = new int[11];
+        for(int i =0; i<n;i++){
+            rolls[rollDie()+rollDie()-2]+=1;
+        }
+        return rolls;
     }
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        Scanner num = new Scanner(System.in);
+        System.out.println("How many pair of dice would you like to roll?");
+        int n = num.nextInt();
+        int[] rolls = sumOfTwoDice(n);
+        for(int b = 0; b<11;b++){
+            System.out.println("% of rolls where sum == "+ (b+2)+ ": "+(((double) rolls[b])/n)*100);
+        num.close();
+        }
+
     }
 }
